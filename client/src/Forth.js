6cfaca004078c3cc7d2374dadd457918b128f54f
Nancy Zaky <nancyzki@yahoo.com>
2021-11-30 10:18:32 -0800

    .

--- client/src/Forth.js
+++ client/src/Forth.js
@@ -2,16 +2,21 @@ import React from "react";
 
 const Forth = () => {
   return (
-    <div>
-      <h3
-        style={{
-          color: "rgb(103, 222, 202)",
-          letterSpacing: "4px",
-          textAlign: "center",
-        }}
-      >
-        4.GLOW UP
-      </h3>
+    <div className="forth-cont">
+      <div style={{ width: "47%", height: "auto", border: "0.2rem solid red" }}>
+        <h3
+          style={{
+            color: "rgb(103, 222, 202)",
+            letterSpacing: "4px",
+            textAlign: "center",
+          }}
+        >
+          4.GLOW UP
+        </h3>
+      </div>
+      <div
+        style={{ width: "47%", height: "auto", border: "0.2rem solid red" }}
+      ></div>
     </div>
   );
 };
