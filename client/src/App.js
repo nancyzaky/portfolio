54b1eaf28842fddf99b331a28629e84960ef0862
Nancy Zaky <nancyzki@yahoo.com>
2021-11-29 19:26:13 -0800

    added packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Main from "./Main";
 import Home from "./Home";
+import Nav from "./Nav";
+import Project from "./Project";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Resume from "./Resume";
 const App = () => {
   return (
     <>
-      <Home />
-      <Main />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Nav />
+            <Home />
+            <Project />
+          </Route>
+
+          <Route exact path="/resume.pdf">
+            <Resume />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
